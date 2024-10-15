a6a1207f366c4ea20a3d99014cf86b649db327af
TNTMGv99 <John.pursell@gmail.com>
2024-10-15 20:04:28 +0100

    Add files via upload

---
+++ batBall.py
@@ -0,0 +1,32 @@
+import pgzrun
+
+WIDTH = 500
+HEIGHT = 500
+
+ball = Rect((150, 400), (60, 20))
+bat = Rect((200, 480), (60, 20))
+vx = 2
+vy = 6
+
+def draw():
+    screen.clear()
+    screen.draw.filled_rect(ball, "red")
+    screen.draw.filled_rect(bat, "white")
+
+def update():
+    global vx, vy
+    ball.x += vx
+    ball.y += vy
+    if ball.right > WIDTH or ball.left < 0:
+        vx = -vx
+    if ball.colliderect(bat) or ball.top < 0:
+        vy= -vy
+    if ball.bottom > HEIGHT:
+        exit()
+    if(keyboard.right):
+        bat.x += 2
+    elif(keyboard.left):
+        bat.x -= 2
+
+
+pgzrun.go()
